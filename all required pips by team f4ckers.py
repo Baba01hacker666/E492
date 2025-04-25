5d12fb08874b9b998d1905cbfcd08a37186e131d
Baba01hacker666 <117832562+Baba01hacker666@users.noreply.github.com>
2025-04-25 14:26:05 +0545

    Add files via upload
    Adding 2k

---
+++ all required pips by team f4ckers.py
@@ -0,0 +1,80 @@
+# This file install all pip required for tools
+
+print('''           
+ /$$   /$$  /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$ 
+| $$  /$$/ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$
+| $$ /$$/ |__/  \ $$| $$  \__/| $$  \__/|__/  \ $$
+| $$$$$/     /$$$$$/|  $$$$$$ | $$$$$$$    /$$$$$/
+| $$  $$    |___  $$ \____  $$| $$__  $$  |___  $$
+| $$\  $$  /$$  \ $$ /$$  \ $$| $$  \ $$ /$$  \ $$
+| $$ \  $$|  $$$$$$/|  $$$$$$/|  $$$$$$/|  $$$$$$/
+|__/  \__/ \______/  \______/  \______/  \______/ 
+          
+ '''.center(10))
+
+#import webbrowser
+#webbrowser.open("https://t.me/teamf4ckers")
+
+import os 
+os.system('pip install OneClick')
+os.system('pip install stdiomask')
+os.system('pip install user_agent')
+os.system('pip install instaloader')
+os.system('pip install requests')
+os.system('pip install rich')
+os.system('pip install telebot')
+os.system('pip install python-cfonts')
+os.system('user_agent')
+os.system('instaloader')
+os.system('mechaize')
+os.system('pip install pyfiglet')
+os.system('pip install sys')
+os.system('pip install py_compile')
+os.system('pip install socks')
+os.system('pip install youtube_dl')
+os.system('pip install uuid')
+os.system('pip install time')
+os.system('pip install os')
+os.system('pip install random')
+os.system('pip install datetime')
+os.system('pip install string')
+os.system('pip install secrets')
+os.system('pip install webbrowser')
+os.system('pip install hashlib')
+os.system('pip install colorama')
+os.system('pip install n')
+os.system('pip install mm')
+os.system('pip install new')
+os.system('pip install sleep')
+os.system('pip install BeautifulSoup')
+os.system('pip install new')
+os.system('pip install pafy')
+os.system('pip install bs4')
+os.system('pip install sys')
+os.system('pip install json')
+os.system('pip install random')
+os.system('pip install uuid')
+os.system('pip install secrets')
+os.system('pip install datetime')
+os.system('pip install *')
+os.system('pip install argparse')
+os.system('pip install InstagramAPI')
+os.system('pip install sleep')
+os.system('pip install string')
+os.system('pip install uuid4')
+os.system('pip install generate_user_agent')
+os.system('pip install threading')
+os.system('pip install json')
+os.system('pip install datetime')
+os.system('pip install token_hex')
+os.system('pip install secrets')
+os.system('pip install token_hex')
+os.system('pip install Fore')
+os.system('pip install secrets')
+os.system('pip install uuid')
+os.system('pip install re')
+os.system('pip install b')
+os.system('cls' if os.name == 'nt' else 'clear') 
+print("≠"*60)
+print("All offices have been completed. Now you can use any tool without a problem or asking you to install a library")
+ #by @teamf4ckers
